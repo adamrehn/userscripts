d39167379de8efe819044a7d1ef2c11ad495b63f
Adam Rehn <adam@adamrehn.com>
2021-02-16 16:18:44 +1000

    Add Centre-Align IETF Drafts and RFCs userscript

---
+++ scripts/centre-align-rfcs.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Centre-Align IETF Drafts and RFCs
+// @namespace    https://adamrehn.com/
+// @version      0.0.1
+// @description  Removes left-alignment of IETF Drafts and RFCs at large resolutions.
+// @author       Adam Rehn
+// @match        https://tools.ietf.org/html/*
+// @require      https://code.jquery.com/jquery-3.5.1.min.js
+// ==/UserScript==
+
+(function()
+{
+	//Apply the same margin value that the page already uses on smaller resolutions (e.g. mobile devices)
+	$('.content').css('margin', '0 auto');
+})();
