b49fef73c9b0b7aada483479612f56dd30daa74f
Adam Rehn <adam@adamrehn.com>
2021-10-31 11:35:08 +1000

    Fix overlay not always being displayed in the YouTube speed script

--- scripts/youtube-speed-controls.user.js
+++ scripts/youtube-speed-controls.user.js
@@ -10,105 +10,111 @@
 
 (function()
 {
-	// Retrieve the video playback element and exit early if it can't be found
-	let videoPlayer = document.getElementsByTagName("video")[0];
-	if (videoPlayer === null || videoPlayer === undefined) {
-		return;
-	}
-	
-	// Create an overlay for displaying playback speed updates
-	let overlay = $(document.createElement('div'));
-	overlay.css('z-index', '9999999');
-	overlay.css('position', 'absolute');
-	overlay.css('left', '2rem');
-	overlay.css('top', '2rem');
-	overlay.css('padding', '2rem');
-	overlay.css('font-size', '2rem');
-	overlay.css('border-radius', '1rem');
-	overlay.css('background', 'rgba(0,0,0,0.5)');
-	overlay.css('color', '#fff');
-	overlay.hide();
-	$('#player-container').append(overlay);
-	
-	// Sets the playback speed for the video
-	let lastTimeout = null;
-	function setPlaybackSpeed(speed)
+	function setup()
 	{
-		// Update the playback speed
-		videoPlayer.playbackRate = speed;
-		
-		// If the overlay is already visible then remove the pending timeout to hide it
-		if (lastTimeout !== null) {
-			clearTimeout(lastTimeout);
+		// Retrieve the video playback element and exit early if it can't be found
+		let videoPlayer = document.getElementsByTagName("video")[0];
+		if (videoPlayer === null || videoPlayer === undefined) {
+			return;
 		}
 		
-		// Display the overlay for 1.5 seconds
-		overlay.text(`Set playback speed to ${speed}x`);
-		overlay.show();
-		lastTimeout = setTimeout(() => {
-			overlay.hide();
-			lastTimeout = null;
-		},
-		1500);
-	}
-	
-	// Wire up our keyboard shortcuts to control playback speed
-	document.addEventListener('keydown', (event) =>
-	{
-		switch (event.key)
+		// Create an overlay for displaying playback speed updates
+		let overlay = $(document.createElement('div'));
+		overlay.css('z-index', '9999999');
+		overlay.css('position', 'absolute');
+		overlay.css('left', '2rem');
+		overlay.css('top', '2rem');
+		overlay.css('padding', '2rem');
+		overlay.css('font-size', '2rem');
+		overlay.css('border-radius', '1rem');
+		overlay.css('background', 'rgba(0,0,0,0.5)');
+		overlay.css('color', '#fff');
+		overlay.hide();
+		$('#player-container ytd-player').append(overlay);
+		
+		// Sets the playback speed for the video
+		let lastTimeout = null;
+		function setPlaybackSpeed(speed)
 		{
-			case '1':
-				setPlaybackSpeed(1);
-				break;
+			// Update the playback speed
+			videoPlayer.playbackRate = speed;
 			
-			case '2':
-				setPlaybackSpeed(1.25);
-				break;
+			// If the overlay is already visible then remove the pending timeout to hide it
+			if (lastTimeout !== null) {
+				clearTimeout(lastTimeout);
+			}
 			
-			case '3':
-				setPlaybackSpeed(1.5);
-				break;
-			
-			case '4':
-				setPlaybackSpeed(1.75);
-				break;
-			
-			case '5':
-				setPlaybackSpeed(2);
-				break;
-			
-			case '6':
-				setPlaybackSpeed(2.5);
-				break;
-			
-			case '7':
-				setPlaybackSpeed(3);
-				break;
-			
-			case '8':
-				setPlaybackSpeed(3.5);
-				break;
-			
-			case '9':
-				setPlaybackSpeed(4);
-				break;
-			
-			case '0':
-				setPlaybackSpeed(0.5);
-				break;
-			
-			default:
-				// Don't interfere with any other keys
-				return true;
+			// Display the overlay for 1.5 seconds
+			overlay.text(`Set playback speed to ${speed}x`);
+			overlay.show();
+			lastTimeout = setTimeout(() => {
+				overlay.hide();
+				lastTimeout = null;
+			},
+			1500);
 		}
 		
-		// Prevent any default behaviour for our shortcut keys
-		event.stopPropagation();
-		event.preventDefault();
-		return false;
-	},
-	{capture: true});
+		// Wire up our keyboard shortcuts to control playback speed
+		document.addEventListener('keydown', (event) =>
+		{
+			switch (event.key)
+			{
+				case '1':
+					setPlaybackSpeed(1);
+					break;
+				
+				case '2':
+					setPlaybackSpeed(1.25);
+					break;
+				
+				case '3':
+					setPlaybackSpeed(1.5);
+					break;
+				
+				case '4':
+					setPlaybackSpeed(1.75);
+					break;
+				
+				case '5':
+					setPlaybackSpeed(2);
+					break;
+				
+				case '6':
+					setPlaybackSpeed(2.5);
+					break;
+				
+				case '7':
+					setPlaybackSpeed(3);
+					break;
+				
+				case '8':
+					setPlaybackSpeed(3.5);
+					break;
+				
+				case '9':
+					setPlaybackSpeed(4);
+					break;
+				
+				case '0':
+					setPlaybackSpeed(0.5);
+					break;
+				
+				default:
+					// Don't interfere with any other keys
+					return true;
+			}
+			
+			// Prevent any default behaviour for our shortcut keys
+			event.stopPropagation();
+			event.preventDefault();
+			return false;
+		},
+		{capture: true});
+		
+		// Set the initial playback speed to 1.0
+		setPlaybackSpeed(1);
+	}
 	
-	// Set the initial playback speed to 1.0
-	setPlaybackSpeed(1);
+	// Wait for all of the page elements to fully load before manipulating the DOM
+	window.setTimeout(setup, 1000);
 })();
