b7f2f10b50023cdee95b64c4988fb2b4da5bdc1f
Adam Rehn <adam@adamrehn.com>
2022-01-28 00:19:48 +1000

    QoL improvements for the hide watched YouTube videos script

--- scripts/youtube-hide-watched.user.js
+++ scripts/youtube-hide-watched.user.js
@@ -10,15 +10,44 @@
 
 (function()
 {
+	function createButton(label)
+	{
+		let button = $(document.createElement('button'));
+		button.css('background-color', 'rgb(6, 95, 212)');
+		button.css('border', '1px solid rgb(6, 95, 212)');
+		button.css('border-radius', '2px');
+		button.css('color', '#fff');
+		button.css('cursor', 'pointer');
+		button.css('font-family', 'Roboto');
+		button.css('font-size', '1.4rem');
+		button.css('font-weight', '500');
+		button.css('padding', '1rem 3rem');
+		button.css('text-transform', 'uppercase');
+		button.text(label);
+		return button;
+	}
+	
 	function setup()
 	{
+		// Create a wrapper <p> to hold our controls
+		let wrapper = $(document.createElement('p'))
+			.css('text-align', 'center')
+			.css('font-size', '1.5rem')
+			.css('padding', '2rem 0')
+			.css('border-bottom', '1px solid rgba(0, 0, 0, 0.1)');
+		
 		// Create our checkbox to toggle the visibility of watched videos
-		let wrapper = $(document.createElement('p')).css('text-align', 'center').css('font-size', '1.5rem').css('margin-top', '2rem');
-		let label = $(document.createElement('label'));
+		let label = $(document.createElement('label')).css('padding', '1rem').css('margin-right', '4rem');
 		let toggle = $(document.createElement('input')).attr('type', 'checkbox').attr('checked', 'checked');
 		label.append(toggle);
 		label.append(document.createTextNode(' Hide watched'));
 		wrapper.append(label);
+		
+		// Create a button that links to the page for managing YouTube watch history
+		let link = $(document.createElement('a')).attr('target', '_blank').attr('href', 'https://myactivity.google.com/u/1/activitycontrols/youtube?utm_source=my-activity');
+		let button = createButton('Manage Watch History');
+		link.append(button);
+		wrapper.append(link);
 		
 		// Updates the visibility of watched videos based on whether the checkbox is checked
 		function updateVisibility()
@@ -35,13 +64,17 @@
 			}
 		}
 		
-		// Inject the checkbox at the top of the page
-		$('#primary').prepend(wrapper);
-		
 		// Wire up the event handler for the checkbox
 		toggle.change(function() {
 			updateVisibility();
 		});
+		
+		// Update the visibility of watched videos whenever new video thumbnails are loaded
+		let observer = new MutationObserver(function() { updateVisibility(); });
+		observer.observe($('#primary').get(0), { childList: true, subtree: true });
+		
+		// Inject the controls at the top of the page
+		$('#primary').prepend(wrapper);
 		
 		// Set the initial visibility of the watched videos
 		updateVisibility();
