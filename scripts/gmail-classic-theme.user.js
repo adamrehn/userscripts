2bc2aa235dd359d52f2b5059923b9117636a5774
Adam Rehn <adam@adamrehn.com>
2022-08-31 18:11:14 +1000

    Add classic Gmail theme script

---
+++ scripts/gmail-classic-theme.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Classic Theme for Gmail
+// @namespace    https://adamrehn.com/
+// @version      0.0.1
+// @description  Modifies Gmail's theme colours to more closely resemble the old Gmail appearance.
+// @author       Adam Rehn
+// @match        https://mail.google.com/*
+// @require      https://code.jquery.com/jquery-3.5.1.min.js
+// ==/UserScript==
+
+(function()
+{
+	var stylesheet = $(document.createElement('style')).attr('type', 'text/css');
+	stylesheet.text(`
+		
+		.wl {
+			background: transparent;
+		}
+		
+		.gb_Se, form.aJf
+		{
+			background: #f1f3f4;
+		}
+		
+		.yO {
+			background: #f4f7f7;
+		}
+		
+		.nH .qp .aJh::placeholder {
+			color: #5e5e5e;
+		}
+		
+	`);
+	$('body').append(stylesheet);
+})();
